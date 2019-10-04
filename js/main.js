d633b314ba9461b73c4610ac30a1a2923d7a4dd4
aterinkov2 <aterinkov@mail.ru>
2019-10-04 23:55:04 +0300

    3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let message = "mamky tvoy ebal sychonish syka blyat eshe sobaky ebal i tebya toshe ebal";
+
 document.querySelector(".is__true").addEventListener("click", function(){
     prompt("ny dolbayob, chi ne?") == "da" ? alert("ny i poshol naxyi") : alert("dashe misli normalno virasit' ne moshes'");
 
@@ -6,16 +8,17 @@ document.querySelector(".is__true").addEventListener("click", function(){
 let texts = document.querySelectorAll(".text");
 let p = 0;
 
-let message = "mamky tvoy ebal sychonish syka blyat eshe sobaky ebal i tebya toshe ebal";
 let arMes = message.split(" ");
 console.log(arMes);
 
 setInterval(() => {
     texts.forEach((item) => {
+        if(p>=arMes.length)
+            p=0;
         item.innerHTML = `${arMes[p]}`;   
-        
+        console.log(p);
     });
-    p++; 
+    ++p; 
 }, 10000);
 
 
