7e45a808f2de1e94fd6b210ab4c19dd9fbe5976d
aterinkov2 <aterinkov@mail.ru>
2019-10-04 23:44:46 +0300

    2

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ document.querySelector(".is__true").addEventListener("click", function(){
     prompt("ny dolbayob, chi ne?") == "da" ? alert("ny i poshol naxyi") : alert("dashe misli normalno virasit' ne moshes'");
 
 });
+
+let texts = document.querySelectorAll(".text");
+let p = 0;
+
+let message = "mamky tvoy ebal sychonish syka blyat eshe sobaky ebal i tebya toshe ebal";
+let arMes = message.split(" ");
+console.log(arMes);
+
+setInterval(() => {
+    texts.forEach((item) => {
+        item.innerHTML = `${arMes[p]}`;   
+        
+    });
+    p++; 
+}, 10000);
+
+
